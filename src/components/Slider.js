989239422b120bf16396c9709497ecf50ce395ef
Dojinkyung <ehwlsrud1@naver.com>
2022-05-08 11:04:00 +0900

    feat:slider button

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,13 +1,16 @@
 import styles from './Slider.module.scss'
 import { useState } from 'react'
 
+const BTN_VAL = [1, 25, 50, 75, 100]
+
 function Slider() {
   const [percent, setPercent] = useState(1)
-
   const onChange = (event) => {
     setPercent(event.currentTarget.value)
   }
-
+  const handleBtnClick = (event) => {
+    setPercent(event.currentTarget.value)
+  }
   return (
     <div>
       <h1>Slider</h1>
@@ -24,6 +27,19 @@ function Slider() {
           step='1'
           style={{ '--percent': `${percent}%` }}
         />
+        <div className={styles.btns}>
+          {BTN_VAL.map((btn) => (
+            <button
+              type='button'
+              key={`btn-key:${btn.toString()}`}
+              className={styles.btn}
+              onClick={handleBtnClick}
+              value={btn}
+            >
+              <div className={styles.btn_style}>{btn}%</div>
+            </button>
+          ))}
+        </div>
       </form>
     </div>
   )
